02f284cd6a59f3c4ed708315f55bf9a967be4230
Johnny Huang <j479huan@uwaterloo.ca>
2021-08-22 20:16:10 -0400

    Changed JSON format to lists of dictionaries and fixed view logic

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
 import sys
 import json
+clear = lambda: os.system('cls')
 
 def mainMenu(): 
+    clear()
     print('''
-        Simple Shoe Inventory Tracking System
-        --------------------------------------
-        1. View Entire Inventory
-        2. View Specific Model Inventory
-        2. Add Item to Inventory
-        3. Remove Item from Inventory
-        4. Edit Items
-        5. Exit
-        ''')
+Simple Shoe Inventory Tracking System
+--------------------------------------
+1. View Entire Inventory
+2. View Specific Model Inventory
+3. Add Item to Inventory
+4. Remove Item from Inventory
+5. Edit Items
+6. Exit
+    ''')
 
     while True:
         option = int(input('Please select one of the following options: '))
@@ -25,32 +27,58 @@ def mainMenu():
             break
 
 def viewInventory():
+    clear()
     with open('inventory.txt', 'r') as inv:
         currInventory = json.loads(inv.read())
+        for name,value in enumerate(currInventory):
+            print(f"{value}: {currInventory[value]}")
     returnToMainMenu()
 
 def viewSpecificInventory():
-    optionShoe = input('Select the shoe model you wish to look at: ').lower()
+    clear()
+    optionSku = input('Select the sku you wish to look at: ').upper()
 
     with open('inventory.txt', 'r') as inv:
         currInventory = json.loads(inv.read())
-        currShoe = currInventory[optionShoe]
 
-        for i in currShoe:
-            print(f"Colourway: {i['Colourway']}\nSize: {i['Size']}\nPrice: {i['Price']}\n")
-
+        if currInventory.get(optionSku) is not None:
+            for i,value in enumerate(currInventory):
+                if(value == optionSku):
+                    currShoe = currInventory[value]
+                    if (len(currShoe) > 1):
+                        for x in range(len(currShoe)):
+                            print(f"Model: {currShoe[x]['model']}\nSize: {currShoe[x]['size']}\nPrice: {currShoe[x]['price']}\n")
+                    elif (len(currShoe) == 1):
+                        print(f"Model: {currShoe[0]['model']}\nSize: {currShoe[0]['size']}\nPrice: {currShoe[0]['price']}")
+        else:
+            print('You currently do not own that SKU')
+            
     returnToMainMenu()
 
-def AddItems():
-    removeItem = input('Name of item you wish to remove: ')
+def addItems():
+    sku = input('Enter the SKU you wish to add: ')
 
     #Check if item exists
 
-    addMore = input ('Would you like to add more items? (y/n): ').lower().strip()
-    if addMore == 'y':
-        AddItems()
-    elif addMore == 'n':
+    removeMore = input ('Would you like to remove more items? (y/n): ').lower().strip()
+    if removeMore == 'y':
+        removeItems()
+    elif removeMore == 'n':
         returnToMainMenu()
+
+def removeItems():
+    sku = input('Enter the SKU you wish to remove: ')
+
+    #Check if item exists
+
+    removeMore = input ('Would you like to remove more items? (y/n): ').lower().strip()
+    if removeMore == 'y':
+        removeItems()
+    elif removeMore == 'n':
+        returnToMainMenu()
+
+def editItems():
+    sku = input('Enter the SKU you wish to edit: ')
     
 
 def returnToMainMenu():
@@ -67,16 +95,3 @@ def exit():
     sys.exit()
 
 mainMenu()
-
-
-
-
-
-
-
-
-
-
-# def mainMenu():
-#     while True:
-#         #insert return to main menu
